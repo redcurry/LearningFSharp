1fd015429dd0c3a1287405ccb52d736786ef542c
Carlos Anderson <carlosja@med.umich.edu>
2019-02-21 19:52:36 -0500

    Add final exercise from chapter 9

--- GetProgrammingwithFSharp/Chapter09.fsx
+++ GetProgrammingwithFSharp/Chapter09.fsx
@@ -4,3 +4,9 @@ let parse (person:string) =
     let playername, game, score = tokens.[0], tokens.[1], tokens.[2]
     let scoreInt = System.Int32.Parse(score)
     playername, game, scoreInt
+
+// Final Ch. 9 Exercise
+let getFilenameAndLastModifiedDate (path:string) =
+    let filename = System.IO.Path.GetFileName(path)
+    let lastModifiedDate = System.IO.File.GetLastWriteTime(path)
+    (filename, lastModifiedDate)
