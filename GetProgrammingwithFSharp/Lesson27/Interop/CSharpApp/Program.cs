2f50d73258de3441f9b0c155f38efb8b67729918
Carlos Anderson <carlosja@med.umich.edu>
2019-03-01 11:32:29 -0500

    Add lesson 27 solution

---
+++ GetProgrammingwithFSharp/Lesson27/Interop/CSharpApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Toyota", Tuple.Create(1.5, 3.5));
+            var bike = Vehicle.NewMotorbike("Bike", 50.0);
+
+            Functions.CreateCar(4, "Toyota", 2, 3);
+            Functions.CreateFourWheeledCar.Invoke("Audi").Invoke(3.0).Invoke(4.0);
+        }
+    }
+}
---
+++ GetProgrammingwithFSharp/Lesson27/Interop/FSharpCode/Library1.fs
@@ -0,0 +1,29 @@
+﻿namespace Model
+
+/// A standard F# record of a Car.
+type Car =
+    {
+      /// The number of wheels on the car
+      Wheels : int
+
+      /// The brand of the car.
+      Brand : string
+      
+      /// The x/y of the car in meters
+      Dimensions : float * float }
+
+/// A vehicle of some sort.
+type Vehicle =
+/// A car is a type of vehicle.
+| Motorcar of Car
+/// A bike is also a type of vehicle
+| Motorbike of Name:string * EngineSize:float
+
+module Functions =
+
+    // Creates a car
+    let CreateCar wheels brand x y =
+        { Wheels = wheels; Brand = brand; Dimensions = x, y }
+
+    // Create a car with four wheels
+    let CreateFourWheeledCar = CreateCar 4
