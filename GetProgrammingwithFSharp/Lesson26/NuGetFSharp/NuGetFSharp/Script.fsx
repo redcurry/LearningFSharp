097063d090dc2e97abd03eff03d1793f2014a565
Carlos Anderson <carlosja@med.umich.edu>
2019-03-01 10:15:39 -0500

    Add Lesson26 solution

---
+++ GetProgrammingwithFSharp/Lesson26/NuGetFSharp/NuGetFSharp/Script.fsx
@@ -0,0 +1,13 @@
+﻿#I @"..\packages"
+
+#r @"Humanizer.Core\lib\netstandard1.0\Humanizer.dll"
+
+open Humanizer
+
+"ScriptsAreAGreatWayToExplorePackages".Humanize()
+
+#r @"Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
+
+#load "Library1.fs"
+
+Library1.getPerson()
