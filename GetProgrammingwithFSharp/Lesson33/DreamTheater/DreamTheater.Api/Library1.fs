728c509e0691277861122e556c284dd033b83a5e
Carlos Anderson <carlosja@med.umich.edu>
2019-03-07 15:43:11 -0500

    Add lesson 33 final exercise solution

---
+++ GetProgrammingwithFSharp/Lesson33/DreamTheater/DreamTheater.Api/Library1.fs
@@ -0,0 +1,21 @@
+﻿namespace DreamTheater.Api
+
+module Songs =
+
+    open FSharp.Data
+
+    type Song =
+        { Title : string
+          Album : string
+          Year : int }
+
+    type private DreamBandProvider = HtmlProvider<"https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Dream_Theater">
+
+    let GetSongs () : Song seq =
+        let dreamBandData = DreamBandProvider.GetSample().Tables.List
+        dreamBandData.Rows
+        |> Seq.map (fun row ->
+            { Title = row.Title
+              Album = row.Album
+              Year = row.Year })
+
---
+++ GetProgrammingwithFSharp/Lesson33/DreamTheater/DreamTheater.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamTheater.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Note: For some reason, I had to edit the project properties
+            // (in Application tab) and choose to auto-generate binding redirects;
+            // something to do with FSharp.Core version conflicts
+            var songs = DreamTheater.Api.Songs.GetSongs();
+        }
+    }
+}
