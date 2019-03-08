a29564ad91f31d8edf94ac4c3ba088b0c3e93b86
Carlos Anderson <carlosja@med.umich.edu>
2019-03-08 09:57:52 -0500

    Add lesson 34 solution

---
+++ GetProgrammingwithFSharp/Lesson34/SqlDemo/DataAccessThroughScript.fsx
@@ -0,0 +1,8 @@
+﻿#r @"..\..\packages\SQLProvider\lib\FSharp.Data.SqlProvider.dll"
+#r @"..\packages\FSharp.Data.SqlClient.2.0.2\lib\net40\FSharp.Data.SqlClient.dll"
+
+#load "CustomerRepository.fs"
+
+let scriptConnectionString = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+
+CustomerRepository.printCustomers(scriptConnectionString)
---
+++ GetProgrammingwithFSharp/Lesson34/SqlDemo/Program.fs
@@ -0,0 +1,7 @@
+﻿open System.Configuration
+
+[<EntryPoint>]
+let main _ =
+    let runtimeConnectionString = ConfigurationManager.ConnectionStrings.["AdventureWorks"].ConnectionString
+    CustomerRepository.printCustomers(runtimeConnectionString)
+    0
